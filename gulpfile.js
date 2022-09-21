d58e80594c0c9204135dd2bb8849cc8529fbe4bd
Trang Vân <trangvan@Trangs-MacBook-Pro.local>
2022-09-21 16:52:08 +0700

    updated sass src

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 gulp.task('sass', function() {
-    return gulp.src('styles/ako.scss')
+    return gulp.src('styles/*.scss')
             .pipe(sass())
             .pipe(gulp.dest('assets'))
 });
